a7b7ae6b4b014f2b0ea47f7bc7ee5e16f9ec762c
Nick Rabone <n.rabone72@aol.com>
2024-06-12 22:42:30 +0100

    Bringing in Daisy UI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,11 @@
+/*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), require('daisyui')],
 };
 
