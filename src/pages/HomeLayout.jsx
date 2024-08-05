171d7db77ec45963bdab9fd54ff1211bae85bee9
Nick Rabone <n.rabone72@aol.com>
2024-08-05 02:34:48 +0100

    Set up all main pages and imported them into main App.js

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,9 @@
+const HomeLayout = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">HomeLayout</h1>
+    </div>
+  )
+}
+
+export default HomeLayout
---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,9 @@
+const SingleProduct = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">SingleProduct</h1>
+    </div>
+  )
+}
+
+export default SingleProduct
