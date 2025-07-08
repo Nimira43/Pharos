0c7ce2a2696a3b47e0b1e9ba6ef7f014a685c9b0
Nimira43 <n.rabone72@aol.com>
2025-07-08 22:40:45 +0100

    Setting output path

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,8 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  }
 }
