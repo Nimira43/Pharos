dbddd1910870e6c3ba740d04d13d6a74fc25dd7e
Nimira43 <n.rabone72@aol.com>
2025-08-04 13:44:01 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ export const router = async () => {
   const app = document.getElementById('app')
   const path = location.pathname || '/'
   const route = routes[path]
+
+  console.log(app)
+  console.log(path)
+  console.log(route)
 }
+
+router()
