8a8fa49ab4c96e15a5614fb8136b3bcb866d627b
Nimira43 <n.rabone72@aol.com>
2025-08-04 13:29:09 +0100

    Created route variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './styles/stylekanban.css'
-import routes from './routes'
+import routes from './routes.js'
 
 export const router = async () => {
   const app = document.getElementById('app')
   const path = location.pathname || '/'
+  const route = routes[path]
 }
