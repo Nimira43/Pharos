f7c04c1a5edf04bbc68bb7e60423273535a56823
Nimira43 <n.rabone72@aol.com>
2025-11-08 11:57:49 +0000

    webpack.config - 3

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,10 @@
-const path = required('path')
+const path = require('path')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    
-  }
+    filename: 'index.js',
+  },
 }
