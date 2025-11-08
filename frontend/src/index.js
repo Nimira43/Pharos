d65836725028207b5b4f7de8b50a681d36b1a298
Nimira43 <n.rabone72@aol.com>
2025-11-08 15:31:21 +0000

    Setting up console logs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,4 +5,8 @@ export const router = async () => {
   const app = document.getElementById('app')
   const path = location.pathname || '/'
   const route = routes[path]
+
+  console.log('app', app)
+  console.log('path', path)
+  console.log('route', route)
 }
