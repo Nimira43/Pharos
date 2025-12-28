f7bb39c8d693ab8d6b4a9e5094943c30543cd4d9
Nimira43 <n.rabone72@aol.com>
2025-12-28 15:18:32 +0000

    bootstrap - 9

--- frontend/src/bootstrap.js
+++ frontend/src/bootstrap.js
@@ -6,12 +6,16 @@ const bootstrap = async (path) => {
       case '/register':
         module = await import('./pages/auth/register.js')
         break
-      case '/login':
-        module = await import('./pages/auth/login.js')
-        break
+      // case '/login':
+      //   module = await import('./pages/auth/login.js')
+      //   break
       default:
         console.log(`No module found for ${path}`)
         break
+    }
+
+    if (module) {
+      console.log('module', module)
     }
 
   } catch (error) {
