6cb51642c4e62a0fe0853cc8975622562bf14430
Nimira43 <n.rabone72@aol.com>
2025-12-28 13:39:40 +0000

    Tweaks made to styling.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,12 @@ const routes = {
   '/': {
     path: '/pages/landing.html',
   },
+  '/register': {
+    path: '/pages/auth/register.html',
+  },
+  '/': {
+    path: '/pages/auth/login.html',
+  },
 }
 
 export default routes
