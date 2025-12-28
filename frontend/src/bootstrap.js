b381123b583a9f4654df2548eeb8877b0fbd6714
Nimira43 <n.rabone72@aol.com>
2025-12-28 15:13:35 +0000

    bootstrap - 5

--- frontend/src/bootstrap.js
+++ frontend/src/bootstrap.js
@@ -2,7 +2,15 @@ const bootstrap = async (path) => {
   try {
     let module
 
+    switch (path) {
+      case value:
+        
+        break
     
+      default:
+        break
+    }
+
   } catch (error) {
     console.log(error, 'Error loading module.')
   }
