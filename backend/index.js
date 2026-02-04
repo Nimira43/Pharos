e07564ea004bd048b94fdcad58d5b360d661fb8a
Nimira43 <n.rabone72@aol.com>
2026-02-04 15:24:22 +0000

    Connected backend to the Mongo DB container.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from 'express'
+import connectDB from './configuration/database.js'
 
 const app = express()
-
 const port = 8000
+
+// Connect to MongoDB
+connectDB()
+
+app.get('/test-route', (req, res) => {
+  res.send('Welcome to Pharos Kanban System.')
+})
 
 app.listen(port, () => {
   console.log(`Welcome to Pharos. Server listening on Port ${port}.`)
 })
 
-app.get('/test-route', (req, res) => {
-  res.send('Welcome to Pharos Kanban System.')
-})
