74dd27f83a49ff9fcb0b3c2a807e83261c9094de
Nimira43 <n.rabone72@aol.com>
2026-05-09 22:50:33 +0100

    Login form validation.

--- frontend/src/pages/auth/login.js
+++ frontend/src/pages/auth/login.js
@@ -16,7 +16,40 @@ class Login {
   async onSubmitLoginForm(e) {
     e.preventDefault()
 
-    console.log('onSubmitLoginForm called when form submitted.')
+    const form = e.target
+
+    const payload = {
+      email: form.querySelector("[name = 'email']").value,
+      password: form.querySelector("[name = 'password']")
+    }
+
+    if (!payload.email || !payload.password) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'All fields are required.',
+        customClass: { confirmButton: 'main-btn' }
+      })
+      return
+    }
+
+    try {
+      console.log('Payload for Backend:', payload)
+      
+      Swal.fire({
+        title: 'Login successful',
+        text: 'You are now authenticated.',
+        icon: 'success',
+        customClass: { confirmButton: 'main-btn' }
+      })
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Server Error. Please try again.',
+        customClass: { confirmButton: 'main-btn' }
+      })
+    }
   }
 
   render() {
