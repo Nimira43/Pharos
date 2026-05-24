8556246cd81583615f35afad250b49e19029eae1
Nimira43 <n.rabone72@aol.com>
2026-05-24 03:12:38 +0100

    AuthStore

---
+++ frontend/src/store/AuthStore.js
@@ -0,0 +1,43 @@
+class AuthStore {
+  constructor() { 
+    this.state = JSON.parse(localStorage.getItem('auth')) || {
+      user: null,
+      isAuthenticated: false
+    }
+  }
+
+  commit(mutation, payload) {
+    if (this.mutations[mutation]) {
+      this.mutations[mutation](this.state, payload)
+      this.saveState()
+    } else {
+      console.log(`This mutation: ${mutation} does not exist.`)
+    }
+  }
+  
+  mutations = {
+    setAuthUser(state, user) {
+      state.user = user
+      state.isAuthenticated = true
+    },
+
+    clearAuthUser(state) {
+      state.user = null
+      state.isAuthenticated = false
+    }
+  }
+
+  saveState() {
+    localStorage.setItem('auth', JSON.stringify(this.state))
+  }
+
+  clearState() {
+    localStorage.removeItem('auth')
+    this.state = { 
+      user: null,
+      isAuthenticated: false
+    }
+  }
+}
+
+export const authStore = new AuthStore()
