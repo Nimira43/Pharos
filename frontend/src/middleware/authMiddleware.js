39bb2b263e6350c8a244a68a88f4006912d0d00c
Nimira43 <n.rabone72@aol.com>
2026-06-11 20:52:38 +0100

    Contining with middlware.

--- frontend/src/middleware/authMiddleware.js
+++ frontend/src/middleware/authMiddleware.js
@@ -10,9 +10,22 @@ export const authMiddleware = (route, next) => {
       icon: 'warning',
       title: 'Wait!',
       text: 'To access Pharos, please log in first.',
+      customClass: { confirmButton: 'main-btn' }
     })
     navigateTo('/login')
     return false
   }
+  
+  if (authState?.isAuthenticated && !route.auth) {
+    Swal.fire({
+      icon: 'info',
+      title: 'You are logged in.',
+      text: 'To access the Register or Login form, please log out first.',
+      customClass: { confirmButton: 'main-btn' }
+    })
+    navigateTo('/dashboard')
+    return false
+  }
+  
   next()
 }
