d465e0cc7bded960081766e5700ec48fa90656f4
Nimira43 <n.rabone72@aol.com>
2026-06-28 21:13:01 +0100

    Created task-model.js

---
+++ backend/models/task-model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const taskSchema = new mongoose.Schema({
+  taskSummary: {
+    type: String,
+    required: true,
+  },
+  acceptanceCriteria: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: 'TO_DO',
+  },
+  // hierarchy: {
+  //   type: Number,
+  //   required: true
+  // },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+})
+
+const Task = mongoose.model('Task', taskSchema)
+export default Task``
